f431999096a7b9f2d48144035de12b5707c3428a
nvazquezg <nvazquezg@gmail.com>
2017-01-18 20:17:13 +0100

    Includes initial BPositive structure and dependencies. Includes images and styles from previous page. Includes a layout template, an example page and a controller to serve home page. Installs new composer dependencies: twbs/bootstrap, components/jquery, components/font-awesome and copy its files to public folder on install or update. Removes Laravel JS, CSS and Vale files that won't be needed. Removes npm's package.json.

---
+++ resources/views/layouts/bpositive.blade.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * B+
+ * Copyright (C) 2017 Jorge Vieira, José Sousa, Miguel Reboiro-Jato,
+ * Noé Vázquez, Bárbara Amorim, Cristina P. Vieira, André Torres, and
+ * Florentino Fdez-Riverola
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>B+ Project - @yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>B+ Project</title>
+    <link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('css/font-awesome.css')}}" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+</head>
+<body>
+    <header>
+        <div class="top-icons">
+            <span class="top-icons-space">
+                <a class="top-icons-link" href="index.php"><i class="fa fa-home fa-lg iconformat_top" title="Home"></i></a>
+                <a class="top-icons-link" href="mailto:jbvieira@ibmc.up.pt"><i class="fa fa-envelope fa-lg iconformat_top" title="Contact us"></i></a>
+                <a class="top-icons-link" href="howtocite.php"><i class="fa fa-pencil-square fa-lg iconformat-top" title="How to cite?" aria-hidden="true"></i></a>
+            </span>
+        </div>
+    </header>
+    <div class="banner">
+        <img src="{{URL::asset('images/bpositive.png')}}" alt="Project Logo" class="project-logo"/>
+    </div>
+    @section('sidebar')
+        This is the master sidebar.
+    @show
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <footer>
+        <div class="footer-left">
+            <a href="http://www.i3s.up.pt"><img src="{{URL::asset('images/i3s.png')}}" alt="i3S"></a>
+        </div>
+        <div class="footer-right">
+            <span>
+                <a class="footer-social-icons" href="https://www.facebook.com/i3s.up.pt/?fref=ts/" target="_new"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a>
+                <a class="footer-social-icons" href="https://twitter.com/i3si3" target="_new"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a>
+                <a class="footer-social-icons" href="https://www.linkedin.com/company/10318110?trk=tyah&trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A10318110%2Cidx%3A1-1-1%2CtarId%3A1479142019953%2Ctas%3Ainstituto%20de%20investiga%C3%A7%C3%A3o%20" target="_new"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a>
+                <a class="footer-social-icons" href="https://pt.pinterest.com/i3suppt/" target="_new"><i class="fa fa-pinterest fa-lg" aria-hidden="true"></i></a>
+            </span>
+            <p class="rodape-info">
+                Phone: +351 220 408 800 <br/>
+                Email: info@i3s.up.pt <br/>
+                Address: Rua Alfredo Allen, 208 <br/>
+                4200-135 Porto, Portugal </p>
+            <span>
+                <a class="footer-icons" href="https://www.ibmc.up.pt" target="_new"><img src="{{URL::asset('images/IBMC.png')}}" alt="IBMC"></a>
+                <a class="footer-icons" href="http://www.ineb.up.pt" target="_new"><img src="{{URL::asset('images/INEB.png')}}" alt="INEB"></a>
+                <a class="footer-icons" href="https://www.ipatimup.pt" target="_new"><img src="{{URL::asset('images/IPATIMUP.png')}}" alt="IPATIMUP"></a>
+                <a class="footer-icons" href="http://www.up.pt" target="_new"><img src="{{URL::asset('images/UPORTO.png')}}" alt="UPORTO"></a>
+            </span>
+        </div>
+    </footer>
+
+    <script type="text/javascript" src="{{URL::asset('js/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{URL::asset('js/bootstrap.min.js')}}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Bpositive\ProjectController@get')->name('projects');
