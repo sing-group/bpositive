f0842f6c6a452a7e75dd6839c3d1c052846de282
nvazquezg <nvazquezg@gmail.com>
2017-02-02 21:43:31 +0100

    Changes Tree View JS library to PhyD3.
    Includes new dependencies mbostock/d4, vibbits/phyd3 and copies the required resources to public folder.
    Renders Tree View with the new library and allows downloading the tree in PNG or SVG format.
    Updates gitignore.

--- resources/views/transcription.blade.php
+++ resources/views/transcription.blade.php
@@ -25,6 +25,10 @@
 @extends('layouts.bpositive')
 
 @section('title', 'Transcription')
+
+@section('startcss')
+    <link href="{{URL::asset('css/phyd3.min.css')}}" rel="stylesheet">
+@endsection
 {{--
 @section('sidebar')
     @parent
@@ -43,7 +47,7 @@
           <h1>{{$transcription->name}}</h1>
         </div>
 
-        <ul class="nav nav-tabs" role="tablist">
+        <ul class="nav nav-tabs" role="tablist" id="topTabs">
             @if(isset($confidences))
                 <li role="presentation" class="active"><a href="#pss" aria-controls="pss" role="tab" data-toggle="tab">PSS</a></li>
             @endif
@@ -54,7 +58,7 @@
             <li role="presentation"><a href="#alnAmin" aria-controls="alnAmin" role="tab" data-toggle="tab">Aligned Amin</a></li>
             <li role="presentation"><a href="#tree" aria-controls="tree" role="tab" data-toggle="tab">Tree</a></li>
             @if(isset($newicks))
-                <li role="presentation"><a href="#treeView" aria-controls="treeView" role="tab" data-toggle="tab">Tree View</a></li>
+                <li role="presentation"><a href="#treeView" id="treeViewTab" aria-controls="treeView" role="tab" data-toggle="tab">Tree View</a></li>
             @endif
             <li role="presentation"><a href="#psrf" aria-controls="psrf" role="tab" data-toggle="tab">PSRF</a></li>
             <li role="presentation"><a href="#codemlOutput" aria-controls="codemlOutput" role="tab" data-toggle="tab">Codeml Output</a></li>
@@ -116,6 +120,8 @@
 @section('endscripts')
     <script type="text/javascript" src="{{URL::asset('js/raphael-min.js')}}" ></script>
     <script type="text/javascript" src="{{URL::asset('js/jsphylosvg-min.js')}}"></script>
+    <script type="text/javascript" src="{{URL::asset('js/d3.min.js')}}"></script>
+    <script type="text/javascript" src="{{URL::asset('js/phyd3.min.js')}}"></script>
     <script type="text/javascript" src="{{URL::asset('js/jspdf.min.js')}}"></script>
     <script type="text/javascript" src="{{URL::asset('js/html2pdf.js')}}"></script>
     <script type="text/javascript" src="{{URL::asset('js/bpositive/pss.js')}}"></script>
@@ -128,40 +134,57 @@
                 for(var i=0; i < dataObjects.length; i++ ){
 
                     var divName = 'svgCanvas' + i;
+                    var data = dataObjects[i];
 
                     var svgCanvas = $("#svgCanvas");
-                    if(i == 0){
-                        $("#svgMenu").append('<li role="presentation" class="active"><a href="#tab' + divName + '" role="tab" data-toggle="tab">Phylogeny ' + i + '</a></li>');
-                        svgCanvas.append('<div id="tab' + divName + '" class="tab-pane fade in active">' +
-                            '<div class="navbar navbar-default"><div class="container-fluid"><form class="navbar-form"><a id="btn' + divName + '" type="button" class="btn btn-info form-control" download="Phylogeny'+ i + '.svg">Download as SVG</a></form></div></div>' +
-                            '<div id="' + divName + '"></div>' +
-                            '</div>');
-                    }
-                    else {
-                        $("#svgMenu").append('<li role="presentation"><a href="#tab' + divName + '" role="tab" data-toggle="tab">Phylogeny ' + i + '</a></li>');
-                        svgCanvas.append('<div id="tab' + divName + '" class="tab-pane fade in">' +
-                            '<div class="navbar navbar-default"><div class="container-fluid"><form class="navbar-form"><a id="btn' + divName + '" type="button" class="btn btn-info form-control" download="Phylogeny'+ i + '.svg">Download as SVG</a></form></div></div>' +
-                            '<div id="' + divName + '"></div>' +
-                            '</div>');
-                    }
 
+                    $("#svgMenu").append('<li role="presentation"><a href="#tab' + divName + '" role="tab" data-toggle="tab" data-newick="' + data + '" data-divname="' + divName + '">Phylogeny ' + i + '</a></li>');
+                    svgCanvas.append('<div id="tab' + divName + '" class="tab-pane fade in">' +
+                        '<div class="navbar navbar-default"><div class="container-fluid"><form class="navbar-form">' +
+                        '<a class="btn btn-info form-control" download="Phylogeny'+ i + '.svg" id="linkSVG">Download as SVG</a>' +
+                        '<a class="btn btn-info form-control" download="Phylogeny'+ i + '.png" id="linkPNG">Download as PNG</a>' +
+                        '<a class="btn btn-default form-control" download="Phylogeny'+ i + '.png" id="resetZoom">Reset zoom</a>' +
+                        //'<div class="checkbox"><label><input id="lengthValues" type="checkbox"><span class="checkbox-material"><span class="check"></span></span> Show length values</label></div>' +
+                        '</form></div></div>' +
+                        '<div id="' + divName + '"></div>' +
+                        '</div>');
+
+
+
+                }
+
+                $('#topTabs').on('shown.bs.tab', function(event){
+                    if($(event.target).html() == $('#treeViewTab').html()) {
+                        $('#svgMenu a:first').tab('show');
+                    }
+                });
+                $('#svgMenu').on('shown.bs.tab', function(event){
+
+                    var tree = phyd3.newick.parse($(event.target).data('newick'));
+                    phyd3.phylogram.build('#' + $(event.target).data('divname'), tree, {
+
+                        showSupportValues: true,
+                        showNodesType: 'all',
+                        nodeHeight: 10
+                    });
+
+                    /*
                     var phylocanvas = new Smits.PhyloCanvas(
-                        dataObjects[i],		// Newick or XML string
-                        divName,	// Div Id where to render
+                        $(event.target).data('newick'),		// Newick or XML string
+                        $(event.target).data('divname'),	// Div Id where to render
                         800, 800		// Height, Width in pixels
 
                         //'circular'
                     );
+                     //TODO: workaround for svg starting with an offset
+                     var svg = $('#' + $(event.target).data('divname') + ' svg');
+                     svg.height(svg.height() + 200);
+                     svg.addClass('center-block');
 
-                    //Gets SVG data and removes all non ASCII characters, because they are problematic for some viewers
-                    $('#btn' + divName).attr('href', 'data:image/svg+xml;base64,' + btoa(phylocanvas.getSvgSource().replace(/[^\x00-\x7F]/g, "")));
-
-                    //TODO: workaround for svg starting with an offset
-                    var svg = $('#' + divName + ' svg');
-                    svg.height(svg.height() + 200);
-                    svg.addClass('center-block');
-
-                }
+                     //Gets SVG data and removes all non ASCII characters, because they are problematic for some viewers
+                     //$('#btn' + divName).attr('href', 'data:image/svg+xml;base64,' + btoa(phylocanvas.getSvgSource().replace(/[^\x00-\x7F]/g, "")));
+                    */
+                });
             @endif
 
             @if(isset($confidences))
@@ -176,6 +199,6 @@
                 );
                 pss.getPSS();
             @endif
-        })
+        });
     </script>
 @endsection('endscripts')
