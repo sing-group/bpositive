fa1cb143829902985cbb53fa6c4e1435ebd287eb
nvazquezg <nvazquezg@gmail.com>
2017-04-22 13:56:42 +0200

    Fixes a problem with language resources.

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -19,7 +19,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-   failed' => 'These credentials do not match our records.',
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
--- resources/lang/en/pagination.php
+++ resources/lang/en/pagination.php
@@ -19,7 +19,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-   previous' => '&laquo; Previous',
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; Previous',
     'next'     => 'Next &raquo;',
 
 ];
